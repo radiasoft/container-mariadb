9fcedf255dd12052cbe740c5f3bf328cf31968b7
robnagler <github@q33.us>
2018-04-13 23:47:59 +0000

    ckp

---
+++ container-conf/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+build_image_base=mariadb:10.2.14
+build_simply=1
+build_docker_cmd='[]'
+build_dockerfile_aux='ENTRYPOINT []'
+
+build_as_root() {
+    cd "$build_guest_conf"
+    build_create_run_user
+    # we generate a single config /etc/my.cnf, and this has higher priority
+    rm -rf /etc/mysql
+}
